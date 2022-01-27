927a7c962643022c5bf39813094f2533e19b2555
sofchet <44215922+sofchet@users.noreply.github.com>
2022-01-27 12:26:18 -0500

    Update transients_extract.m

--- source/transients_extract.m
+++ source/transients_extract.m
@@ -1,3 +1,9 @@
+% Copyright (c) 2021, SCALE Lab, Brown University
+% All rights reserved.
+
+% This source code is licensed under the license found in the
+% LICENSE file in the root directory of this source tree. 
+
 function [transient_state] = extract_transients(data, change_ind_trans)
 % Extracts transient traces from data by for all indices
 % :inputs:
